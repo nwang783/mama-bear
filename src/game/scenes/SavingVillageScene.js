fb4fdcde1bf583914731cbe11675468105adddea
nwang <nathanwang@Nathans-MacBook-Air.local>
2025-10-26 10:00:19 -0400

    Updated villages

--- src/game/scenes/SavingVillageScene.js
+++ src/game/scenes/SavingVillageScene.js
@@ -3,19 +3,19 @@ import House from '../entities/House';
 import { GAME_CONFIG } from '../config/gameConfig';
 
 /**
- * Math Village Scene - contains math-related minigame houses
+ * Saving Village Scene - contains saving-related minigame houses
  */
-export default class MathVillageScene extends VillageScene {
+export default class SavingVillageScene extends VillageScene {
   constructor() {
-    super('MathVillageScene');
+    super('SavingVillageScene');
   }
 
   createHouses() {
-    // Get math village house configurations
-    this.housesConfig = GAME_CONFIG.HOUSES.MATH_VILLAGE;
+    // Get saving village house configurations
+    this.housesConfig = GAME_CONFIG.HOUSES.SAVING_VILLAGE;
     
     // Get village color
-    const villageColor = GAME_CONFIG.VILLAGES.find(v => v.id === 'math').color;
+    const villageColor = GAME_CONFIG.VILLAGES.find(v => v.id === 'saving').color;
 
     // Create each house
     this.housesConfig.forEach(houseConfig => {
